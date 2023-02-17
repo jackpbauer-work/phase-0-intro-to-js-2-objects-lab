ab48923952dee518b517dbce510b4e086f0f2c82
Jack Bauer <jackbauer.work@gmail.com>
2023-02-16 22:22:19 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    name:"Jack", 
+    streetAddress:"123 Central Street"
+  };
+function updateEmployeeWithKeyAndValue(object, key, value) {
+   let copyObject = {...object};
+   copyObject[key] = value;
+   return copyObject
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object
+}
+function deleteFromEmployeeByKey(object, key) {
+    var newObj = Object.assign({}, object);
+    delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key]
+    return object
+}
+
